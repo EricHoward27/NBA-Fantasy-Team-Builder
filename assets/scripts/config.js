ecb168b3d73d314a405f5a22a9b2e850c90bbbf6
Eric <howarderic585@gmail.com>
2021-03-10 13:20:33 -0600

    fix config file to deploy

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://still-hamlet-42818.herokuapp.com/',
+  production: 'https://still-hamlet-42818.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
