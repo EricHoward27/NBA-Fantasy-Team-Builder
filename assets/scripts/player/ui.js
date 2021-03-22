85b15ba478e2be48cf81f319203e762e5d292e7c
Eric <howarderic585@gmail.com>
2021-03-22 11:05:24 -0500

    fix deploy

--- assets/scripts/player/ui.js
+++ assets/scripts/player/ui.js
@@ -2,34 +2,35 @@ const store = require("../store")
 
 const addPlayerSuccess = (data) => {
  
-$('#error-message').text('Player added successful')
+$('#player-message').text('Player added successful')
 
+store.player = data.player
 setTimeout(() => {
-    $('#error-message').hide()
+    $('#player-message').hide()
 }, 4000)
 $('form').trigger('reset')
 }
 const addPlayerFail = () => {
-    $('#error-message').text('Error, please try again.')
+    $('#player-message').text('Error, please try again.')
     
     setTimeout(() => {
-        $('#error-message').hide()
+        $('#player-message').hide()
     }, 4000)
 }
 const updatePlayerSuccess = (data) => {
-    $('#error-message').text('You successfully updated player!')
+    $('#player-message').text('You successfully updated player!')
 
     // console.log('Update player data is ', data)
 setTimeout(() => {
-    $('#error-message').hide()
+    $('#player-message').hide()
 }, 4000)
 $('form').trigger('reset')
 }
 const updatePlayerFail = (data) => {
-    $('#error-message').text('Error, please try again.')
+    $('#player-message').text('Error, please try again.')
 
     setTimeout(() => {
-        $('#error-message').hide()
+        $('#player-message').hide()
     }, 4000)
 }
 
